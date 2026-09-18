0c5e88fa26df033a4831a1dbf7b32c7ff370e5ef
Menezesvm <vinicius.menezes2@ufrpe.br>
2026-09-18 16:59:42 -0200

    Obtendo um post por id

---
+++ src/main/java/com/menezesvm/estudosmongodb/controller/PostController.java
@@ -0,0 +1,24 @@
+package com.menezesvm.estudosmongodb.controller;
+
+import com.menezesvm.estudosmongodb.domain.Post;
+import com.menezesvm.estudosmongodb.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostController {
+
+	@Autowired
+	private PostService postService;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = postService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
---
+++ src/main/java/com/menezesvm/estudosmongodb/service/PostService.java
@@ -0,0 +1,21 @@
+package com.menezesvm.estudosmongodb.service;
+
+import com.menezesvm.estudosmongodb.domain.Post;
+import com.menezesvm.estudosmongodb.repository.PostRepository;
+import com.menezesvm.estudosmongodb.service.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
