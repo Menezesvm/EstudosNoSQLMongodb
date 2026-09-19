aeff7f40e19e86eb79857db3dbdccc7e879f7140
Menezesvm <vinicius.menezes2@ufrpe.br>
2026-09-19 18:52:56 -0200

    Consulta simples com query methods

--- src/main/java/com/menezesvm/estudosmongodb/controller/PostController.java
+++ src/main/java/com/menezesvm/estudosmongodb/controller/PostController.java
@@ -1,13 +1,16 @@
 package com.menezesvm.estudosmongodb.controller;
 
+import com.menezesvm.estudosmongodb.controller.util.URL;
 import com.menezesvm.estudosmongodb.domain.Post;
 import com.menezesvm.estudosmongodb.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -20,5 +23,11 @@ public class PostController {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = postService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByIdTitle(@RequestParam(value = "text", defaultValue = "") String text) throws UnsupportedEncodingException {
+		text = URL.decodeParam(text);
+		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
